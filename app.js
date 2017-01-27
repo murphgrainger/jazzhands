f365bd5e44518e4728de6822695e8df11b9c0b62
Alex <alexanderlperez@gmail.com>
2017-01-26 19:49:43 -0700

    updated to 3 words at a time"

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 ;(function ($) {
     $(document).ready(function () {
-        var words = ['ba', 'be', 'bi', 'bodda', 'bop', 'bope', 'da', 'dib', 'dibby', 'dub', 'im', 'scatman', 'ski', 'the', 'yo'];
 
         function newWord() {
+            var words = ['ba', 'be', 'bi', 'bodda', 'bop', 'bope', 'da', 'dib', 'dibby', 'dub', 'im', 'scatman', 'ski', 'the', 'yo'];
+
             function pickWord() {
                 return words[Math.floor(Math.random() * words.length)]
             }
 
             $('#game-field').empty();
 
-            var word = pickWord()
+            var words3 = [];
+            for( i = 0; i < 3 ; i++  ) {
+                var pick = pickWord();
+                console.log(pick);
+                words3.push(pick);
+            }
+
+            console.log(words3);
+
+            var word = words3.join( ' ' );
 
             for (var l = 0 ; l < word.length; l++) {
                 var char = word.charAt(l);
-                var $char = $('<span class="l' + char +'"></span>').text(char);
+
+                var $char = $('<span></span>').text(char);
                 $('#game-field').append($char);
+
+                if (char !== ' ') {
+                    $char.addClass( 'l' + char)
+                }
             }
 
             return word;
@@ -24,6 +39,8 @@
 
         var word = newWord();
         var score = 0;
+
+        console.log(word);
 
 
         $(document).on('keydown', function (e) {
@@ -40,7 +57,11 @@
 
             console.log(countHighlighted);
 
-            if (countHighlighted == word.length) {
+            var properWord = word.replace(/\W/g, '')
+            console.log('proper word length', properWord.length);
+            console.log('proper word', properWord);
+
+            if (countHighlighted == properWord.length) {
                 word = newWord();
                 $('#score').text(++score);
             }
